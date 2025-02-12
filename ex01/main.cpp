394429d8ab35b93fc23a60474fb9f33faba9b7c3
kaveOO <alban.billiette@gmail.com>
2025-02-12 03:24:11 +0100

    + | Finished ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/02 11:03:05 by albillie          #+#    #+#             */
-/*   Updated: 2025/02/11 19:20:57 by albillie         ###   ########.fr       */
+/*   Created: 2025/02/11 22:09:25 by albillie          #+#    #+#             */
+/*   Updated: 2025/02/12 03:21:31 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 int main( void )
 {
 	Fixed a;
-	a.setRawBits(11111.00000000000000);
-	Fixed b( a );
-	Fixed c;
+	Fixed const b( 10 );
+	Fixed const c( 42.4434433f );
+	Fixed const d( b );
 
-	c = b;
+	a = Fixed( 1234.43232249f );
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
 	return 0;
 }
